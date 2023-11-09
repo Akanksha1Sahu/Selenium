4804dcad0a41e42fdcbfab1ebfaa08d6d1f65193
Akanksha Sahu <146106315+Akanksha1Sahu@users.noreply.github.com>
2023-11-09 15:13:42 -0500

    updated GreenKart class code

--- SeleniumBasics/src/GreenKart.java
+++ SeleniumBasics/src/GreenKart.java
@@ -1,6 +1,8 @@
+import java.time.Duration;
 import java.util.Arrays;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 
@@ -9,6 +11,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GreenKart {
 
@@ -17,6 +21,8 @@ public class GreenKart {
 				"C:\\Users\\rohit\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
+		// driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
 
 		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
 
@@ -25,6 +31,21 @@ public class GreenKart {
 		Thread.sleep(3000);
 
 		addItems(driver, itemsNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+
+		// explicit wait
+
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 
 	}
 
